328b589824386d91fc59ce679399582d25e9210a
vadim rusu <ruvamd@gmail.com>
2023-11-10 20:46:06 -0800

    .

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 #users/urls.py
 
-from django.urls import path
+from django.urls import path, include
 from users.views import dashboard
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', dashboard, name='dashboard'),
 ]
