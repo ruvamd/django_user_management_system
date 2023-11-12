140f92045a2b83d4a593a4081c433f6aaab4265f
vadim rusu <ruvamd@gmail.com>
2023-11-11 17:13:11 -0800

    done

--- users/views.py
+++ users/views.py
@@ -1,7 +1,24 @@
-#users/views.py
+# users/views.py
 
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from users.forms import CustomUserCreationForm
 
-# Create your views here.
 def dashboard(request):
-    return render(request, 'users/dashboard.html')
+    return render(request, "users/dashboard.html")
+
+def register(request):
+    if request.method == "GET":
+        return render(
+            request, "users/register.html",
+            {"form": CustomUserCreationForm}
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.backend = "django.contrib.auth.backends.ModelBackend"
+            user.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
